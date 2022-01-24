0007cbf368b7115743ba25c6f9d6483b0200b6ba
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-01-24 19:42:12 +0530

    .

--- watermelon.c
+++ watermelon.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
-void main()
+int main()
 {
     int w;
-    printf("enter the weight of watermelon in kg:");
     scanf("%d",&w);
-    if(w % 2 == 0)
-    printf("the watermelon can be divided into two parts of even weight");
-    else
-    printf("the watermelon can't be divided into two parts of even weight");
+    if(w <= 2 || w%2 != 0){
+        printf("NO");
+    }
+    else{
+        printf("YES");
+    }
+    return 0;
 }
