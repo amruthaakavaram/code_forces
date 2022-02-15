3c5f53bf081550e1133eb8ea5990521bb221e556
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-02-15 23:41:33 +0530

    .

---
+++ 236_A.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    
+    const char str[100], X;
+    scanf("%s",&X);
+    strlen(X);
+    if(strlen(X)%2==0){
+        printf("CHAT WITH HER!");
+        }
+    else{
+        printf("IGNORE HIM!");
+    }
+    
+
+}
